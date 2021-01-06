9a630b1c26e012a7edac5ad593b41bcdd9f47071
錦麻以 <mai@myiMac.local>
2021-01-06 23:15:26 +0900

    imprement component with connect function

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,26 @@
 import React, { Component } from 'react';
+import { connect} from 'react-redux';
 
-function App() {
-  return <div><h1>Hello, world!</h1></div>;
+import { increment, decrement } from '../actions';
+
+class App extends Component {
+  render() {
+    const props = this.props;
+    return (
+      <React.Fragment>
+        <div>value: { props.value }</div>
+        <button onClick={props.increment}>+1</button>
+        <button onClick={props.decrement}>-1</button>
+      </React.Fragment>
+    )
+  }
 }
 
-export default App;
+const mapStateToProps = state => ({ value: state.count.value });
+
+const mapDispatchToProps = dispatch => ({
+  increment: () => dispatch(increment()),
+  decrement: () => dispatch(decrement())
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
