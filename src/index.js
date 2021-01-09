6157b3d8162c56c93245a07805daa0dd4f419cb2
nskmy <maitakeking@gail.com>
2021-01-09 19:11:32 +0900

    imprement events new component

--- src/index.js
+++ src/index.js
@@ -3,16 +3,24 @@ import ReactDOM from 'react-dom';
 import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
 import thunk from 'redux-thunk';
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
+
 import reducer from './reducers';
 import './index.css';
 import EventsIndex from './components/events_index';
-import reportWebVitals from './reportWebVitals';
+import EventsNew from './components/events_new';
+import registerServiceWorker from './registerServiceWorker';
 
 const store = createStore(reducer, applyMiddleware(thunk));
 
 ReactDOM.render(
   <Provider store={store}>
-    <EventsIndex />
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/events/new" component={EventsNew} />
+        <Route exact path="/" component={EventsIndex} />
+      </Switch>
+    </BrowserRouter>
   </Provider>,
   document.getElementById('root')
 );
@@ -20,4 +28,4 @@ ReactDOM.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+registerServiceWorker();
